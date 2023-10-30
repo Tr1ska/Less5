21457c2fd28230d38ce69a699dd3affb4c0d1b2c
315-09 <nat36712@gmail.com>
2023-10-30 17:45:50 +0200

    All projectss

--- is_class.py
+++ is_class.py
@@ -3,6 +3,7 @@ class First_class:
 
 class Second_class(First_class):
     pass
+obj = Second_class()
 
-print(issubclass(First_class, Second_class))
-print(issubclass(Second_class, First_class))
+print(isinstance(obj, Second_class))
+print(isinstance(obj, First_class))
